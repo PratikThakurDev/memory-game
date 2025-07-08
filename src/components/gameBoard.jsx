da4b144b903afdfb431fc9f5c5881f80ade146d4
PratikThakurDev <149695885+PratikThakurDev@users.noreply.github.com>
2025-07-08 17:35:32 +0000

    Fixed the bug of only shown clicked at some point of the game

--- src/components/gameBoard.jsx
+++ src/components/gameBoard.jsx
@@ -3,19 +3,42 @@ import '../styles/gameBoard.css';
 
 function GameBoard({ setPlayingMode }) {
     const [allCharacters, setAllCharacters] = useState([]);
+    const [clickedChar, setClickedChar] = useState([]);
 
     function handleHomeClick() {
         setPlayingMode(false);
     }
 
-    function shuffleCards() {
-        const arr = [...allCharacters];
+    function shuffleCards(e) {
+        const { target } = e;
 
-        for (let i = arr.length - 1; i > 0; i--) {
-            const j = Math.floor(Math.random() * (i + 1));
-            [arr[i], arr[j]] = [arr[j], arr[i]];
+        const arr = [...allCharacters];
+        let unclicked = [];
+
+        // keep shuffling until there's at least one unclicked character
+        while (true) {
+            // shuffle
+            for (let i = arr.length - 1; i > 0; i--) {
+                const j = Math.floor(Math.random() * (i + 1));
+                [arr[i], arr[j]] = [arr[j], arr[i]];
+            }
+
+            // find unclicked cards
+            unclicked = arr.filter((char) => {
+                for (let j = 0; j < clickedChar.length; j++) {
+                    if (char.mal_id == clickedChar[j]) {
+                        return false;
+                    }
+                }
+                return true;
+            });
+
+            // exit loop if unclicked found
+            if (unclicked.length !== 0) break;
         }
+
         setAllCharacters(arr);
+        setClickedChar([...clickedChar,e.target.dataset.id])
     }
 
     useEffect(() => {
@@ -46,7 +69,12 @@ function GameBoard({ setPlayingMode }) {
 
             <div className="cardGrid">
                 {allCharacters.slice(0, 12).map((char) => (
-                    <div className="card" key={char.mal_id} onClick={shuffleCards}>
+                    <div
+                        className="card"
+                        key={char.mal_id}
+                        data-id={char.mal_id}
+                        onClick={shuffleCards}
+                    >
                         <img src={char.images.jpg.image_url} alt={char.name} />
                         <p>{char.name}</p>
                     </div>
